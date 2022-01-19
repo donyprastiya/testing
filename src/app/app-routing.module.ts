7282b6e59f112a7395be98744c763815a144c9cd
Dony Prastiya <donyprastiya@gmail.com>
2022-01-19 11:09:31 +0700

    heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Contoh1Component } from './contoh1/contoh1.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', redirectTo:'/login', pathMatch:'full'},
+  {path : '/login', component:ReactiveFormComponent},
+  {path : '/contoh1',component:Contoh1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
